3287c7123a9a41bec8b1ea87415b1d35e714dc12
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-30 12:19:58 -0400

    Updated default '/' route so that it retrieves data from the database instead of using the static data in the file.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+const itemsTable = "items"
+
 const db = new pg.Client({
   user: process.env.PG_USERNAME,
   host: "localhost",
@@ -24,19 +26,29 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// variable to store all of the items
 let items = [
   // { id: 1, title: "Buy milk" },
   // { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+async function getAllItems (){
+  const result = await db.query(`SELECT * FROM ${itemsTable}`);
+  console.log('getAllItems(): result = ', result);
+  return result.rows;
+}
+
+// default home page route
+app.get("/", async (req, res) => {
+  items = await getAllItems();
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
   items.push({ title: item });
   res.redirect("/");
